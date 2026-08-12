e19b01de8708207ea9e04cc84afbd6fa383080cb
GemmaChapman <gemma.chapman@engineering.digital.dwp.gov.uk>
2026-08-12 12:28:56 +0100

    fixes
    GitOrigin-RevId: 645eecf4f06850037c7b99e53bb6ded91e20c26e

--- app/routes.js
+++ app/routes.js
@@ -13,7 +13,7 @@ router.post('*', function (req, res, next) {
   console.log(req.body);
 
   if (req.body['next-page']) {
-    res.redirect('/');
+    res.redirect(req.body['next-page']);
   } else {
     next();
   }
